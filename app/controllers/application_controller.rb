d840295babefd62083f587cf92a761ec62e62f1c
Ancor Cruz <ancor@osl.ull.es>
2008-09-04 12:54:52 +0000

    Remodelacion del header del template por un header cercano al final (OSL like).
    ligeras modificaciones en la presentacion de la lista de aplicaciones y en la vista de aplicacion.
    aplicados cambios menores en la hoja de estilo que afecta al aspecto general.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,5 +7,4 @@ class ApplicationController < ActionController::Base
   # See ActionController::RequestForgeryProtection for details
   # Uncomment the :secret if you're not using the cookie session store
   protect_from_forgery # :secret => 'a8cd8350fd7769a825b7fc67f19a29de'
-
 end
