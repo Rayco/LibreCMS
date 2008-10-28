6be3a4f2351c2bc3359ea605a7fe79d8b1fdf4d0
Ancor Cruz <ancor@osl.ull.es>
2008-10-28 14:18:24 +0000

    Fijada version de Rails para evitar futuros errores por actualizaciones de Rails.
    Intentando corregir el bug de edicion de categorias pero de momento no doy con la solucion.

--- app/models/category.rb
+++ app/models/category.rb
@@ -6,11 +6,11 @@ class Category < ActiveRecord::Base
   validates_presence_of :name
   validates_uniqueness_of :name, :case_sensitive => false, :message => 'Ya existe'
   
-  #validates_attachment_content_type :icon, :content_type => ['image/jpeg', 'image/pjpeg', 'image/gif', 'image/png', 'image/x-png', 'image/jpg']
-  
   has_attached_file :icon, :styles => { :normal => "32x32", :big => "128x128", :small => "16x16" },
                     :url => "/attached/:class/:id/:attachment/:style_:basename.:extension",
                     :path => ":rails_root/public/attached/:class/:id/:attachment/:style_:basename.:extension",
                     :default_style => :normal,
                     :default_url => "/images/defaults/:class_:attachment_:style.png"
+                    
+  validates_attachment_content_type :icon, :content_type => ['image/jpeg', 'image/pjpeg', 'image/gif', 'image/png', 'image/x-png', 'image/jpg']
 end
