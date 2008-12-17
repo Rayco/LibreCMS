617e4d5c0f7618f193fe891eff3325d59774d331
Ancor Cruz <ancor@osl.ull.es>
2008-12-17 10:44:44 +0000

    Remove all references to platforms in installer model, views (and shared view).

--- app/models/installer.rb
+++ app/models/installer.rb
@@ -2,13 +2,9 @@ require 'paperclip'
 
 class Installer < ActiveRecord::Base
   belongs_to :application
-  belongs_to :platform
   
   validates_presence_of :application_id
   validates_associated :application
-  
-  validates_presence_of :platform_id
-  validates_associated :platform
   
   has_attached_file :installer,
                     :url => "/attached/:class/:id/:attachment/:basename.:extension",
