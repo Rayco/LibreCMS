f2b824277e4026cc3bdb8c5cfc7f44cca0893adb
Ancor Cruz <ancor@osl.ull.es>
2009-03-03 14:09:48 +0000

    Arregladas las vistas para que ahora se aproveche mejor el espacio usable, extendiendo las zonas de contenido y reduciendo espacios en blanco.

--- app/controllers/applications_controller.rb
+++ app/controllers/applications_controller.rb
@@ -16,7 +16,7 @@ class ApplicationsController < ApplicationController
   # GET /applications/1.xml
   def show
     @application = Application.find_by_name(params[:id].from_url)
-    @screenshots = @application.screenshots.paginate :per_page => 1, :page => params[:page]
+    @screenshots_list = @application.screenshots.paginate :per_page => 1, :page => params[:page]
 
     respond_to do |format|
       format.html # show.html.erb
