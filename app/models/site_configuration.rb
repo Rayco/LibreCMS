4149ceeb3e3ace43c8f45aca6337d03c0e6ba477
Rayco Abad-Martín <rayco@osl.ull.es>
2009-12-21 19:08:02 +0000

    Se han tenido que quitar las validaciones del Site_Configuration, así como volver a pedir el root category id a la hora de crear un nuevo site, ya que al realizar una instalación limpia se detectaban errores tanto en las migraciones como en la creación del catálogo inicial

--- app/models/site_configuration.rb
+++ app/models/site_configuration.rb
@@ -3,9 +3,4 @@ class SiteConfiguration < ActiveRecord::Base
   belongs_to :root_category, :class_name => 'Category'
   has_many :relations_in_site, :foreign_key => 'site_id', :class_name => 'MenuNode', :dependent => :destroy
 
-  # Validates
-  validates_uniqueness_of :website, :case_sensitive => false, :message => 'Ya existe'
-  validates_format_of :website, :with => /^\w+([-.]\w+)*\.\w{2,}$/i
-  validates_format_of :webiso, :with => /^(http[s]?:\/\/\w+([-.]\w+)*\.\w{2,}(\/\w+([-.]\w+)*)+\.iso)?$/i
-
 end
