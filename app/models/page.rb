83c099afde099503c99a49b02f172b6d63425e8a
Rayco Abad-Martín <rayco@osl.ull.es>
2010-01-19 14:41:04 +0000

    Cambios menores en las vistas de pages

--- app/models/page.rb
+++ app/models/page.rb
@@ -1,13 +1,5 @@
 class Page < ActiveRecord::Base
-  @site = SiteConfiguration.find(:all)
-
   # Validates
-  $site_name = "";
-  for site in @site
-    $site_name += " " + site.name
-  end
-  validates_format_of :site, :with => /($site_name)?/i
   validates_format_of :permalink, :with => /^\w+([-]\w+)*$/i
-  validates_uniqueness_of :permalink, :case_sensitive => false, :message => 'Ya existe'
-
+  validates_uniqueness_of :permalink, :case_sensitive => false, :message => 'exists'
 end
