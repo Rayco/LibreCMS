00aa30524e960c85f01769fae3e4db89d14a377c
Rayco Abad-Martín <rayco@osl.ull.es>
2010-01-20 10:31:59 +0000

    Validaciones para Site Configurations

--- app/models/site_configuration.rb
+++ app/models/site_configuration.rb
@@ -3,4 +3,7 @@ class SiteConfiguration < ActiveRecord::Base
   belongs_to :root_category, :class_name => 'Category'
   has_many :relations_in_site, :foreign_key => 'site_id', :class_name => 'MenuNode', :dependent => :destroy
 
+  validates_presence_of :name
+  validates_presence_of :website
+
 end
