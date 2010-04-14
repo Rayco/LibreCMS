e78001095b93c4ab8f7842d0753aeeed6eaf9b98
Rayco Abad-Martín <rayco@osl.ull.es>
2010-04-14 10:33:10 +0000

    Se hace un merge del branch search_12042010 versión 278 a la versión del trunk 287

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -7,16 +7,35 @@ class SearchController < ApplicationController
   # GET /search
   # GET /search.xml
   def create
-    @search = Application.find(:all, :conditions => ["name LIKE ? OR license LIKE ?", "%#{params[:s]}%", "%#{params[:s]}%"])
-    @search += Application.find_by_sql(['select * from applications where id IN 
-						(select taggable_id from taggings where tag_id LIKE 
-							(select id from tags where name LIKE ? ))', "%#{params[:s]}%"]);
-    @search += Application.find(:all, :conditions => ["description LIKE ?", "%#{params[:s]}%"])
-    @search = @search.uniq
+    if params[:s].length > 1 
+      params[:s] = translate(params[:s])
+      @search = Application.find(:all, :conditions => ["name LIKE ? OR license LIKE ?", "%#{params[:s]}%", "%#{params[:s]}%"])
+      @search += Application.find_by_sql(['select * from applications where id IN 
+                                            (select taggable_id from taggings where tag_id LIKE 
+                                              (select id from tags where name LIKE ? ))', "%#{params[:s]}%"]);
+      @search += Application.find(:all, :conditions => ["description LIKE ?", "%#{params[:s]}%"])
+      @search = @search.uniq
+    end
   end
 
   def show
     redirect_to root_path 
+  end
+
+  def translate (parameters)
+    parameters = parameters.gsub('Ñ', '&ntilde;')
+    parameters = parameters.gsub('Á', '&aacute;')
+    parameters = parameters.gsub('É', '&eacute;')
+    parameters = parameters.gsub('Í', '&iacute;')
+    parameters = parameters.gsub('Ó', '&oacute;')
+    parameters = parameters.gsub('Ú', '&uacute;')
+    parameters = parameters.gsub('ñ', '&ntilde;')
+    parameters = parameters.gsub('á', '&aacute;')
+    parameters = parameters.gsub('é', '&eacute;')
+    parameters = parameters.gsub('í', '&iacute;')
+    parameters = parameters.gsub('ó', '&oacute;')
+    parameters = parameters.gsub('ú', '&uacute;')
+    return parameters
   end
 
 end
